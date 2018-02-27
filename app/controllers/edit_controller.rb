27b1007134866a553a352e5274477c57e17821f6
Viddesh Gharpure <viddesh.gharpure@gmail.com>
2018-02-27 21:31:19 +0530

    Added route to Video Edits Page.

---
+++ app/controllers/edit_controller.rb
@@ -0,0 +1,5 @@
+class EditController < ApplicationController
+  def home
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#home'
+  get '/home', to: 'home#home'
+  get '/edits', to: 'edit#edit'
 
 end
